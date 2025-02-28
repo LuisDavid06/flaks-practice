bac32ba193b9dda512bdfc187a6c8a28455e3466
luisdavid_lamar <luisdavidortegasoto@gmail.com>
2025-02-28 16:02:17 -0400

    finished tuto

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from markupsafe import escape
 from datetime import datetime
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Length
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = 'DEV'
+)
 # filtros
 @app.add_template_filter
 def today(date):
@@ -12,6 +17,9 @@ def today(date):
 
 @app.route('/')
 def index():
+    print(url_for('index'))
+    print(url_for('hello', name="luis"))
+    print(url_for('code', code ='print("hola")'))
     name = "Luis David"
     frineds = ['Jose', 'Juan', 'Lisandro', 'Ana','Otras mas' ]
     date = datetime.now()
@@ -21,14 +29,45 @@ def index():
 @app.route('/hello')
 @app.route('/hello/<string:name>')
 @app.route('/hello/<string:name>/<int:age>')
-def hello(name = None, age= None):
-    if name == None and age == None:
-        return '<h1> Hello world</h1>'
-    elif age == None:
-        return f'<h1>Hello {name}</h1>'
-    else:
-        return f'<h1>hola {name}, tu edad {age}!!</h1>'
+@app.route('/hello/<string:name>/<int:age>/<email>')
+def hello(name = None, age= None, email= None):
+    my_data = {
+        'name':name,
+        'age':age,
+        'email':email
+    }
+
+    return render_template('hello.html', data= my_data)
         
 @app.route('/code/<path:code>')
 def code(code):
     return f'hola, {escape(code)}'
+
+# crear formulario wtfrom
+
+class RegisterForm(FlaskForm):
+    username = StringField("Nombre de usuario:", validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField("Password: ", validators=[DataRequired(), Length(min=6, max=25)])
+    submit = SubmitField("Registrar")
+
+
+# registrar usuarios
+@app.route('/auth/register', methods=['GET', 'POST'])
+def registrar_usuarios():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return f"Nombre de usuario{username}, Contraseña: {password}"
+
+
+    # if request.method == 'POST':
+    #     username = request.form['username']
+    #     password = request.form['password']
+
+    #     if len(username)>= 4 and len(username) <= 25 and len(password) >= 6 and len(password) <= 40:
+    #         return f"Nombre de usuario{username}, Contraseña: {password}"
+    #     else:
+    #         error = """Nombre de usuarii debe tener 4 y 25 caracteres, tambien la contraseña debe tener entre 6 y 25 caracteres"""
+    #         return render_template('auth/register.html', form = form,  error = error)
+    return render_template('auth/register.html', form = form)
